2727a424dbf88c7d8d08a9c7271119b900368412
edaSkmn <edasekmen@windowslive.com>
2023-01-14 16:06:00 +0300

    Version1

---
+++ src/test/java/CampusTest/US_2_Attestations.java
@@ -0,0 +1,22 @@
+package CampusTest;
+
+import org.testng.annotations.BeforeClass;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.baseURI;
+
+public class US_2_Attestations {
+
+    @BeforeClass
+    public void loginCampus() {
+
+        baseURI = "https://test.mersys.io/";
+        Map<String, String> credential = new HashMap<>();
+        credential.put("username", "turkeyts");
+        credential.put("password", "TechnoStudy123");
+        credential.put("rememberMe", "true");
+    }
+
+    }
---
+++ src/test/java/Classes/US_2_Attestations.java
@@ -0,0 +1,8 @@
+package Classes;
+
+public class US_2_Attestations {
+
+    private String name;
+    private String code;
+    private String id;
+}
